e4f00eb3c328e45259ef0f721b676253e9472184
Lakshya Pandey <74096902+lakshyapandey1250@users.noreply.github.com>
2020-11-18 18:53:12 +0530

    Made The Code More Readable

--- Weather App.py
+++ Weather App.py
@@ -1,13 +1,17 @@
+# ----- Imports ----- 
 import tkinter as tk
 import requests
 
+# ----- Global Variables -----
 HEIGHT = 500
 WIDTH = 600
 
 # api.openweathermap.org/data/2.5/weather?q={city name},{country code}
 # Get Your API Key From openweatherapp.org
 
+# ----- Functions -----
 def format_response(weather):
+	"""Formats The HTTP Response To A String"""
 	try:
 		name = weather['name']
 		desc = str(weather['weather'][0]['description']).capitalize()
@@ -20,6 +24,7 @@ def format_response(weather):
 	return final_str
 
 def get_weather(city):
+	"""Gets The Weather From The Input"""
 	weather_key = 'api_key'
 	url = 'https://api.openweathermap.org/data/2.5/weather'
 	params = {'APPID': weather_key, 'q': city, 'units': 'metric'}
@@ -28,15 +33,14 @@ def get_weather(city):
 
 	label['text'] = format_response(weather)
 
+# ----- Main Code -----
 
-
+# Initializing The Main Tkinter Window
 root = tk.Tk()
-
 root.title("Weather App")
+root.geometry(f"{WIDTH}x{HEIGHT}")
 
-canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
-canvas.pack()
-
+# Initializing And Setting The Images
 logo_image = tk.PhotoImage(file="Logo.png")
 background_image = tk.PhotoImage(file='Background.png')
 root.iconphoto(False, logo_image)
@@ -44,13 +48,14 @@ root.iconbitmap("Icon.ico")
 background_label = tk.Label(root, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
+# Creating The Main Window
 frame = tk.Frame(root, bg='#80c1ff', bd=5)
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
 entry = tk.Entry(frame, font=("Times New Roman", 15))
 entry.place(relwidth=0.65, relheight=1)
 
-button = tk.Button(frame, text="Get Weather", font=("Times New Roman", 15), command=lambda: get_weather(entry.get()))
+button = tk.Button(frame, text="Get Weather", font=("Times New Roman", 15), command=lambda:get_weather(entry.get()))
 button.place(relx=0.7, relheight=1, relwidth=0.3)
 
 lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
@@ -59,4 +64,6 @@ lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
 label = tk.Label(lower_frame,font=("Courier",18))
 label.place(relwidth=1, relheight=1)
 
-root.mainloop()
+# ----- Driver Code -----
+if __name__ == "__main__":
+	root.mainloop()
