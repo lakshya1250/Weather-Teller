5c4797cedc15eb4959a55616d7882682a5c8d529
Lakshya Pandey <74096902+lakshyapandey1250@users.noreply.github.com>
2020-12-24 02:16:59 +0530

    Made The Code More Readable

--- Weather Teller.py
+++ Weather Teller.py
@@ -1,4 +1,5 @@
 # ----- Imports ----- 
+from tkinter import *
 import tkinter as tk
 import requests
 
@@ -8,7 +9,7 @@ width = 600
 background_color = "#80C1FF"
 
 # Get Your API Key From openweatherapp.org
-# api.openweathermap.org/data/2.5/weather?q={city name},{country code}
+# api.openweathermap.org/data/2.5/weather?q = {city name}, {country code}
 
 # ----- Functions -----
 def format_response(weather):
@@ -29,7 +30,7 @@ def get_weather(city):
 	weather_key = 'api_key'
 	url = 'https://api.openweathermap.org/data/2.5/weather'
 	params = {'APPID': weather_key, 'q': city, 'units': 'metric'}
-	response = requests.get(url, params=params)
+	response = requests.get(url, params = params)
 	weather = response.json()
 
 	label1['text'] = format_response(weather)
@@ -42,29 +43,29 @@ root.title("Weather App")
 root.geometry(f"{width}x{height}")
 
 # Initializing And Setting The Images
-logo_image = tk.PhotoImage(file="Logo.png")
-background_image = tk.PhotoImage(file='Background.png')
+logo_image = tk.PhotoImage(file = "Logo.png")
+background_image = tk.PhotoImage(file = 'Background.png')
 root.iconphoto(False, logo_image)
 root.iconbitmap("Icon.ico")
-background_label = tk.Label(root, image=background_image)
-background_label.place(relwidth=1, relheight=1)
+background_label = tk.Label(root, image = background_image)
+background_label.place(relwidth = 1, relheight = 1)
 
 # Creating The Main Window
-frame1 = tk.Frame(root, bg=background_color, bd=5)
-frame1.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+frame1 = tk.Frame(root, bg = background_color, bd = 5, relief = RIDGE)
+frame1.place(relx = 0.5, rely = 0.1, relwidth = 0.75, relheight = 0.1, anchor = 'n')
 
-entry1 = tk.Entry(frame1, font=("Times New Roman", 20))
-entry1.place(relwidth=0.65, relheight=1)
+entry1 = tk.Entry(frame1, font = ("Times New Roman", 20), relief = RIDGE)
+entry1.place(relx = 0.02, rely = 0.1, relwidth = 0.6, relheight = 0.8)
 
-button1 = tk.Button(frame1, text="Get Weather", font=("Times New Roman", 20), command=lambda:get_weather(entry.get()), cursor="hand2")
-button1.place(relx=0.7, relheight=1, relwidth=0.3)
+button1 = tk.Button(frame1, text = "Get Weather", font = ("Times New Roman", 18), relief = RIDGE, command = lambda:get_weather(entry1.get()), cursor = "hand2")
+button1.place(relx = 0.68, rely = 0.1, relwidth = 0.3, relheight = 0.8)
 
-frame2 = tk.Frame(root, bg=background_color, bd=10)
-frame2.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+frame2 = tk.Frame(root, bg = background_color, bd = 10, relief = RIDGE)
+frame2.place(relx = 0.5, rely = 0.25, relwidth = 0.75, relheight = 0.6, anchor = 'n')
 
-label1 = tk.Label(frame2,font=("Courier",20))
-label1.place(relwidth=1, relheight=1)
+label1 = tk.Label(frame2, font = ("Courier", 18), relief = RIDGE)
+label1.place(relwidth = 1, relheight = 1)
 
 # ----- Driver Code -----
-if __name__ == "__main__":
+if __name__ =  = "__main__":
 	root.mainloop()
